889146daa25bd97766122dd6f9a3a982b882d9b8
Артем  Шитиков <aea.apr@mail.ru>
2026-06-04 04:15:21 +0700

    1

---
+++ README.md
@@ -0,0 +1,14 @@
+# Yatube API
+
+API для Yatube. Посты, комментарии, подписки, группы.
+
+## Запуск
+
+```bash
+pip install -r requirements.txt
+cd yatube_api
+python manage.py migrate
+python manage.py runserver
+```
+
+Документация: http://127.0.0.1:8000/redoc/
---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,62 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Follow, Group, Post
+
+User = get_user_model()
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+
+    def validate(self, data):
+        if self.context['request'].user == data.get('following'):
+            raise serializers.ValidationError(
+                {'following': 'Нельзя подписаться на самого себя!'}
+            )
+        return data
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+            )
+        ]
