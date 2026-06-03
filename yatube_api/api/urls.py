19c2726e35b9aba1ec3c9402caa905e44c16e9eb
Артем  Шитиков <aea.apr@mail.ru>
2026-06-04 04:20:29 +0700

    Fix

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,7 +4,8 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView,
                                             TokenVerifyView)
 
-from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+from api.views import (CommentViewSet, FollowViewSet,
+                       GroupViewSet, PostViewSet)
 
 router = DefaultRouter()
 router.register('posts', PostViewSet)
@@ -28,7 +29,19 @@ urlpatterns = [
         }),
         name='comments-detail'
     ),
-    path('v1/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path(
+        'v1/jwt/create/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'v1/jwt/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'v1/jwt/verify/',
+        TokenVerifyView.as_view(),
+        name='token_verify'
+    ),
 ]
